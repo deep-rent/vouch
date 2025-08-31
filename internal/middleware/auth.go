0868950c54eeb99b9310701a88c9d0f69187a62b
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-08-31 17:14:03 +0200

    fix: skip auth for cors preflight

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -4,6 +4,10 @@ import "net/http"
 
 func auth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		if req.Method == http.MethodOptions {
+			next.ServeHTTP(res, req)
+			return
+		}
 		next.ServeHTTP(res, req)
 	})
 }
