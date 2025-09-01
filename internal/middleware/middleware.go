cae63945a264f49e9882dd1d667bfb7c132043ad
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-09-01 14:08:42 +0200

    refactor: define type for middleware signature

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -2,13 +2,16 @@ package middleware
 
 import "net/http"
 
+// Middleware wraps a HTTP handler to form a middleware chain.
+type Middleware func(http.Handler) http.Handler
+
 // Apply applies all middleware to the given handler.
 func Apply(h http.Handler) http.Handler {
 	return chain(h, auth)
 }
 
 // chain composes middleware handlers (outermost first).
-func chain(h http.Handler, mws ...func(http.Handler) http.Handler) http.Handler {
+func chain(h http.Handler, mws ...Middleware) http.Handler {
 	for i := len(mws) - 1; i >= 0; i-- {
 		h = mws[i](h)
 	}
