95f4adf7fe11d4c783a576ef3b9bf4c2269d9638
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-09-04 15:26:36 +0200

    test: add unit tests for signer

---
+++ internal/hash/signer_test.go
@@ -0,0 +1,25 @@
+// Copyright (c) 2025-present deep.rent GmbH (https://www.deep.rent)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hash
+
+import "testing"
+
+func TestMatch(t *testing.T) {
+	exp := "027da48c8c642ca4c58eb982eec81915179e77a3"
+	s := New("secret")
+	if act := s.Sign("user"); act != exp {
+		t.Errorf("Sign() = %v, want %v", act, exp)
+	}
+}
