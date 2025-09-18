7f62cacba2201c0fd5cd077764cca489d0ee4239
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-09-18 19:23:09 +0200

    docs: add documentation comments

--- internal/auth/guard.go
+++ internal/auth/guard.go
@@ -5,10 +5,17 @@ import (
 	"net/http"
 )
 
+// Guard checks incoming HTTP requests for proper authentication and
+// authorization prior to proxying them to CouchDB.
 type Guard interface {
+	// Permits inspects the given HTTP request and writes an appropriate HTTP
+	// response if the request is not authorized. It returns true if the request
+	// is permitted to proceed to CouchDB.
 	Permits(w http.ResponseWriter, r *http.Request) bool
 }
 
+// NewGuard creates a new Guard using the given Bouncer and Stamper.
+// Additional options can be provided to customize its behavior.
 func NewGuard(bouncer Bouncer, stamper Stamper, opts ...GuardOption) Guard {
 	g := &guard{
 		bouncer: bouncer,
@@ -21,14 +28,21 @@ func NewGuard(bouncer Bouncer, stamper Stamper, opts ...GuardOption) Guard {
 	return g
 }
 
+// guard is the default implementation of Guard.
+// It uses a Bouncer to authorize requests and a Stamper to attach CouchDB
+// user context to them.
 type guard struct {
 	bouncer Bouncer
 	stamper Stamper
 	logger  *slog.Logger
 }
 
+// GuardOption configures a Guard.
 type GuardOption func(*guard)
 
+// WithLogger sets a custom logger for the Guard.
+//
+// If nil is given, this option is ignored. By default, slog.Default() is used.
 func WithLogger(logger *slog.Logger) GuardOption {
 	return func(g *guard) {
 		if logger != nil {
@@ -37,10 +51,13 @@ func WithLogger(logger *slog.Logger) GuardOption {
 	}
 }
 
+// Permits implements the Guard interface.
 func (g *guard) Permits(w http.ResponseWriter, r *http.Request) bool {
 	logger := g.logger.With("method", r.Method, "path", r.URL.Path)
 	access, err := g.bouncer.Check(r)
 
+	// Currently, AccessError must be top-level. If this changes, we may need to
+	// switch to errors.As to find it in the error stack.
 	if e, ok := err.(*AccessError); ok {
 		logger.Debug("Access denied", "error", e)
 		w.WriteHeader(e.StatusCode())
