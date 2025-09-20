d2950d9ec4cdb9d779794db02c4917d67712992f
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-09-20 13:13:07 +0200

    feat: implement config loading

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,52 @@
 package config
 
-type Config struct{}
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/goccy/go-json"
+	"github.com/goccy/go-yaml"
+)
+
+type Config struct {
+	// Config data...
+}
+
+// Decoder decodes raw configuration data into the provided Go value.
+type Decoder func(data []byte, v any) error
+
+// infer detects the Decoder to use based on the file extension.
+// Returns nil if no suitable Decoder is found.
+func infer(path string) Decoder {
+	switch strings.ToLower(filepath.Ext(path)) {
+	case ".json":
+		return json.Unmarshal
+	case ".yaml", ".yml":
+		return yaml.Unmarshal
+	default:
+		return nil
+	}
+}
+
+// Load reads the configuration file from the given path, decodes it,
+// and returns a populated Config struct by value.
+// If dec is nil, it attempts to infer the decoder from the file extension.
+func Load(path string, dec Decoder) (Config, error) {
+	if dec == nil {
+		if dec = infer(path); dec == nil {
+			return Config{}, errors.New("unsupported format")
+		}
+	}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return Config{}, fmt.Errorf("reading failed: %w", err)
+	}
+	var cfg Config
+	if err := dec(data, &cfg); err != nil {
+		return Config{}, fmt.Errorf("parsing failed: %w", err)
+	}
+	return cfg, nil
+}
