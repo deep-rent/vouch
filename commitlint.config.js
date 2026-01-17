a1da2c69328c11ce1064c83e2fb84c4f7d52f35f
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-01-17 17:30:57 +0100

    chore: disable commitlint integration

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('@commitlint/types/lib').UserConfig} */
-export default {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "scope-enum": [2, "always", []],
-  },
-};
