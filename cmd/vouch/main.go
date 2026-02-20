881bc4cf3913bb4142b4905e12179cac963a4c3a
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-02-20 15:12:39 +0100

    feat: define main application runner

--- cmd/vouch/main.go
+++ cmd/vouch/main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/deep-rent/nexus/app"
+	"github.com/deep-rent/nexus/log"
+)
+
+func main() {
+	logger := log.New(
+		// TODO: Fetch log level and format from environment configuration
+		log.WithLevel("info"),
+		log.WithFormat("text"),
+	)
+
+	runnable := func(ctx context.Context) error {
+		// Starting application
+		<-ctx.Done()
+		// Shutting down
+		return nil
+	}
+
+	if err := app.Run(runnable, app.WithLogger(logger)); err != nil {
+		logger.Error("Failed to run application", slog.Any("error", err))
+		os.Exit(1)
+	}
+}
