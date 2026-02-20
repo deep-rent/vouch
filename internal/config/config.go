498db2850c80b4d62eed6299ec5fdd863d77d743
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-02-20 11:32:27 +0100

    feat: outline config structure

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,4 +1,13 @@
 package config
 
 type Config struct {
+	JWKS       string   `env:",required"`
+	UserAgent  string   `env:",default=Vouch"`
+	RoleClaim  string   `env:",default:_couchdb.roles"`
+	Audiences  []string `env:",split"`
+	Issuers    []string `env:",split"`
+	Leeway     int      `env:",default=0"`
+	MaxAge     int      `env:",default=0"`
+	UserHeader string   `env:",default:X-Auth-CouchDB-UserName"`
+	RoleHeader string   `env:",default:X-Auth-CouchDB-Roles"`
 }
