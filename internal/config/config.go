80efe5906fe9e4b5b0dd77b86b6e05b451ff79e2
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-02-21 14:12:31 +0100

    refactor: remove old config options

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,29 +1,6 @@
 package config
 
-import (
-	"log/slog"
-	"net/url"
-	"time"
-)
-
 type Config struct {
-	LogLevel           string
-	LogFormat          string
-	Host               string
-	Port               string
-	Target             *url.URL
-	JWKS               string
-	Issuers            []string
-	Audiences          []string
-	Leeway             time.Duration
-	MaxAge             time.Duration
-	UserAgent          string
-	Timeout            time.Duration
-	MinRefreshInterval time.Duration
-	MaxRefreshInterval time.Duration
-	AuthScheme         string
-	RolesClaim         string
-	UserHeader         string
-	RolesHeader        string
-	Logger             *slog.Logger
+	LogLevel  string
+	LogFormat string
 }
