969f9aab36e607712b0981683d6428d4064f743e
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-02-21 22:00:31 +0100

    test: compare all issuers

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -28,13 +28,13 @@ func TestLoad(t *testing.T) {
 		"VOUCH_MAX_BUFFER_SIZE":            "8192",
 		"VOUCH_MAX_IDLE_CONNS":             "100",
 		"VOUCH_IDLE_CONN_TIMEOUT":          "60",
-		"VOUCH_TOKEN_ISSUERS":              "https://a.com,https://b.issuer.com",
+		"VOUCH_TOKEN_ISSUERS":              "https://issuer-1.com,https://issuer-2.com",
 		"VOUCH_TOKEN_AUDIENCES":            "api,admin",
 		"VOUCH_TOKEN_LEEWAY":               "5",
 		"VOUCH_TOKEN_MAX_AGE":              "3600",
 		"VOUCH_TOKEN_AUTH_SCHEME":          "OAuth",
 		"VOUCH_TOKEN_ROLES_CLAIM":          "roles",
-		"VOUCH_KEYS_URL":                   "https://c.com/.well-known/jwks.json",
+		"VOUCH_KEYS_URL":                   "https://issuer.com/.well-known/jwks.json",
 		"VOUCH_KEYS_USER_AGENT":            "Vouch-Test",
 		"VOUCH_KEYS_TIMEOUT":               "5",
 		"VOUCH_KEYS_MIN_REFRESH_INTERVAL":  "10",
@@ -61,7 +61,7 @@ func TestLoad(t *testing.T) {
 	assert.Equal(t, "X-Roles", cfg.RolesHeader)
 	assert.Equal(t, "OAuth", cfg.TokenAuthScheme)
 	assert.Equal(t, "roles", cfg.TokenRolesClaim)
-	assert.Equal(t, "https://c.com/.well-known/jwks.json", cfg.KeysURL)
+	assert.Equal(t, "https://issuer.com/.well-known/jwks.json", cfg.KeysURL)
 	assert.Equal(t, "Vouch-Test", cfg.KeysUserAgent)
 	assert.Equal(t, 1048576, cfg.MaxHeaderBytes)
 	assert.Equal(t, 4096, cfg.MinBufferSize)
@@ -81,16 +81,13 @@ func TestLoad(t *testing.T) {
 	assert.Equal(t, 60*time.Minute, cfg.KeysMaxRefreshInterval)
 	assert.Equal(t, 2*time.Second, cfg.KeysBackoffMinDelay)
 	assert.Equal(t, 30*time.Second, cfg.KeysBackoffMaxDelay)
-
-	require.NotNil(t, cfg.Target)
+	assert.NotNil(t, cfg.Target)
 	assert.Equal(t, "http://couchdb:5984", cfg.Target.String())
-
 	assert.Len(t, cfg.TokenIssuers, 2)
-	assert.Equal(t, "https://a.com", cfg.TokenIssuers[0])
-
+	assert.Equal(t, "https://issuer-1.com", cfg.TokenIssuers[0])
+	assert.Equal(t, "https://issuer-2.com", cfg.TokenIssuers[1])
 	assert.Len(t, cfg.TokenAudiences, 2)
 	assert.Equal(t, "api", cfg.TokenAudiences[0])
-
 	assert.Equal(t, 2.5, cfg.KeysBackoffGrowthFactor)
 	assert.Equal(t, 0.1, cfg.KeysBackoffJitterAmount)
 }
