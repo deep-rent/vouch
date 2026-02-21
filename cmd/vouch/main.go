3569e0d013c56ed7545b1951eeb16274b131507b
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-02-21 13:25:38 +0100

    refactor: pass main config options

--- cmd/vouch/main.go
+++ cmd/vouch/main.go
@@ -3,20 +3,43 @@ package main
 import (
 	"context"
 	"os"
+	"time"
 
 	"github.com/deep-rent/nexus/app"
 	"github.com/deep-rent/nexus/log"
+	"github.com/deep-rent/vouch/internal/bouncer"
+	"github.com/deep-rent/vouch/internal/gateway"
 	"github.com/deep-rent/vouch/internal/server"
+	"github.com/deep-rent/vouch/internal/stamper"
 )
 
 func main() {
-	logger := log.New(
-		log.WithLevel("info"),
-	)
+	logger := log.New(log.WithLevel("info"), log.WithFormat(log.FormatJSON))
 
 	runnable := func(ctx context.Context) error {
+		bouncer := bouncer.New(&bouncer.Config{
+			JWKS: "",
+		})
+
+		stamper := stamper.New(&stamper.Config{
+			UserNameHeader: "X-Auth-CouchDB-UserName",
+			RolesHeader:    "X-Auth-CouchDB-Roles",
+		})
+
+		gateway := gateway.New(&gateway.Config{
+			Bouncer: bouncer,
+			Stamper: stamper,
+		})
+
 		s := server.New(&server.Config{
-			Logger: logger,
+			Handler:           gateway,
+			Host:              "",
+			Port:              "8080",
+			ReadHeaderTimeout: time.Second,
+			ReadTimeout:       time.Second,
+			WriteTimeout:      time.Second,
+			IdleTimeout:       time.Second,
+			Logger:            logger,
 		})
 		go s.Start()
 		<-ctx.Done()
